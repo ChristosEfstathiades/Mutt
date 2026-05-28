b619977e4222fadaf3f3ea1cff10c463daea1e63
d86617ce <chrise3012@icloud.com>
2026-05-28 22:22:59 +0100

    init

---
+++ Mutt.md
@@ -0,0 +1,7 @@
+# Mutt
+
+## Steps
+
+1. lexical analyser - split tokens input into tokens
+2. Parser - create AST according to grammar - recursive descent
+3. Backend - Translates to C
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cstdint>
+#include "Runtime.hpp"
+
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+
+using i8 = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+
+int main() {
+    std::string contents;
+    {
+        std::fstream file("../MuttSrc/main.mutt");
+        std::stringstream contents_stream;
+        contents_stream << file.rdbuf();
+        contents = contents_stream.str();
+    }
+    std::cout << contents << std::endl;
+    return 0;
+}
